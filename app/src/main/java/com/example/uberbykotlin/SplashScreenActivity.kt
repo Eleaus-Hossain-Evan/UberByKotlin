8b17f7669a9e452f1e47aed73081de0dca6fdd69
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-12-02 03:27:52 +0600

    implement SplashScreenActivity.kt

--- app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
@@ -2,9 +2,17 @@ package com.example.uberbykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Completable
+import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Completable.timer(3, TimeUnit.SECONDS)
+            .subscribe({
+                Toast.makeText(this@SplashScreenActivity, "Splash Screen", Toast.LENGTH_SHORT).show()
+            })
     }
 }
