0fb9c17bfcae62e82ca327db536f791fcdb98f3b
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-12-02 03:09:23 +0600

    add splash image

--- app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
@@ -3,9 +3,8 @@ package com.example.uberbykotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
