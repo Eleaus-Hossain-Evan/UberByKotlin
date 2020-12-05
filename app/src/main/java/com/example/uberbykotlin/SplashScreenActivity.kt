46c0ddc376d25a2530ed7d9536ef759884bd9bdc
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-12-06 02:52:19 +0600

    Complete btnRegister

--- app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/example/uberbykotlin/SplashScreenActivity.kt
@@ -1,8 +1,9 @@
 package com.example.uberbykotlin
 
 import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -15,18 +16,13 @@ import com.example.uberbykotlin.model.DriverInfoModel
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.material.internal.ContextUtils.getActivity
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import io.reactivex.Completable
 import kotlinx.android.synthetic.main.activity_splash_screen.*
-import kotlinx.android.synthetic.main.layout_register.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -34,10 +30,11 @@ import java.util.concurrent.TimeUnit
 class SplashScreenActivity : AppCompatActivity() {
 
 
-    companion object{
+    companion object {
         private val LOGIN_REQUEST_CODE = 7171
     }
 
+    private lateinit var context1: Context
     private lateinit var providers: List<AuthUI.IdpConfig>
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var listener: FirebaseAuth.AuthStateListener
@@ -58,7 +55,7 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     override fun onStop() {
-        if (firebaseAuth != null && listener != null){
+        if (firebaseAuth != null && listener != null) {
             firebaseAuth.removeAuthStateListener(listener)
         }
         super.onStop()
@@ -73,6 +70,7 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        context1 = this.window.context
         firebaseAuth = FirebaseAuth.getInstance()
 
         database = FirebaseDatabase.getInstance()
@@ -83,95 +81,107 @@ class SplashScreenActivity : AppCompatActivity() {
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
-        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth->
+        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth ->
             user = myFirebaseAuth.currentUser!!
-            if (user != null){
+            if (user != null) {
                 checkUserFromFirebase()
-            }
-            else {
+            } else {
                 showLoginLayout()
             }
         }
     }
 
     private fun checkUserFromFirebase() {
-        FirebaseDatabase.getInstance().reference
-            .child("DriverInfo")
-            .child(user.uid)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        if (snapshot.exists()) {
-                            Toast.makeText(
-                                applicationContext,
-                                "User already registered!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            Log.d("User-Registered", "user allready registerd"+ user.uid)
-                        } else {
-                            showRegisterLayput()
-                        }
-                    }
+        val rootRef = FirebaseDatabase.getInstance().reference
+        val userNameRef = rootRef.child(Common.DRIVER_INFO_REFERENCE)
+        val queries = userNameRef.orderByChild("uid").equalTo(user.uid)
+        val eventListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (!dataSnapshot.exists()) {
+                    showRegisterLayout()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "User already registered!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d("User-Registered", "user already registered" + user.uid)
                 }
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    Toast.makeText(applicationContext, "" + error.message, Toast.LENGTH_LONG)
-                        .show()
-                }
-            })
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(
+                    applicationContext,
+                    "-------DatabaseError: " + databaseError.message,
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+            }
+        }
+        queries.addListenerForSingleValueEvent(eventListener)
     }
 
-    private fun showRegisterLayput() {
-        val builder = AlertDialog.Builder(applicationContext, R.style.DialogTheme)
-        val itemView = LayoutInflater.from(applicationContext).inflate(
-            R.layout.layout_register,
-            null
-        )
+    private fun showRegisterLayout() {
+        val builder = AlertDialog.Builder(this)
+        lateinit var dialog: AlertDialog
+        builder.setTitle("REGISTER ")
+        builder.setMessage(R.string.registerText)
 
-        val edtFirstName = itemView.findViewById<View>(R.id.edtFirstName) as TextInputEditText
-        val edtLastName = itemView.findViewById<View>(R.id.edtLastName) as TextInputEditText
-        val edtPhone = itemView.findViewById<View>(R.id.edtPhoneNumber) as TextInputEditText
-
-        val btnContinue = itemView.findViewById<View>(R.id.btnRegister)
+        val inflater = LayoutInflater.from(this)
+        val itemView = inflater.inflate(R.layout.layout_register, null)
+        val edtFirstName: TextInputEditText = itemView.findViewById(R.id.edtFirstName)
+        val edtLastName: TextInputEditText = itemView.findViewById(R.id.edtLastName)
+        val edtPhoneNumber: TextInputEditText = itemView.findViewById(R.id.edtPhoneNumber)
+        val btnReg: MaterialButton = itemView.findViewById(R.id.btnRegister)
 
         //Set data
-        if(FirebaseAuth.getInstance().currentUser!!.phoneNumber != null &&
-                !TextUtils.isDigitsOnly(firebaseAuth.currentUser!!.phoneNumber)) {
-            edtPhone.setText(firebaseAuth.currentUser!!.phoneNumber)
+        if (FirebaseAuth.getInstance().currentUser!!.phoneNumber != null &&
+            !TextUtils.isDigitsOnly(firebaseAuth.currentUser!!.phoneNumber)
+        ) {
+            edtPhoneNumber.setText(firebaseAuth.currentUser!!.phoneNumber)
         }
-
-        //Set view
-        builder.setView(itemView)
-        val dialog = builder.create()
-        dialog.show()
-
         //Event
-        btnContinue.setOnClickListener{
-            if(TextUtils.isDigitsOnly(edtFirstName.text.toString())){
-                Toast.makeText(applicationContext, "Please enter First name", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }else if(TextUtils.isDigitsOnly(edtLastName.text.toString())){
-                Toast.makeText(applicationContext, "Please enter Last name", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }else if(TextUtils.isDigitsOnly(edtPhone.text.toString())){
-                Toast.makeText(applicationContext, "Please enter Phone number", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }else{
+        btnReg.setOnClickListener {
+            if (TextUtils.isDigitsOnly(edtFirstName.text.toString())) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter First name",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else if (TextUtils.isDigitsOnly(edtLastName.text.toString())) {
+                Toast.makeText(applicationContext, "Please enter Last name", Toast.LENGTH_SHORT)
+                    .show()
+
+            } else if (TextUtils.isDigitsOnly(edtPhoneNumber.text.toString())) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter Phone number",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+                dialog.dismiss()
                 val model = DriverInfoModel()
                 model.firstName = edtFirstName.text.toString().trim()
                 model.lastName = edtLastName.text.toString().trim()
-                model.phone = edtPhone.text.toString().trim()
+                model.phone = edtPhoneNumber.text.toString().trim()
                 model.rating = 0.0
 
                 driverInfoRef.child(FirebaseAuth.getInstance().currentUser!!.uid)
                     .setValue(model)
                     .addOnFailureListener {
-                        Toast.makeText(applicationContext, "" + it.message, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            applicationContext,
+                            "" + it.message,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
                         Log.d("__register", "" + it.message)
                         dialog.dismiss()
                         progressBar.visibility = View.GONE
                     }
-                    .addOnSuccessListener{
+                    .addOnSuccessListener {
                         Toast.makeText(
                             applicationContext,
                             "Registered Successfully!",
@@ -184,7 +194,10 @@ class SplashScreenActivity : AppCompatActivity() {
                     }
             }
         }
-
+        //Set view
+        builder.setView(itemView)
+        dialog= builder.create()
+        dialog.show()
     }
 
     private fun showLoginLayout() {
@@ -206,12 +219,12 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == LOGIN_REQUEST_CODE){
+        if (requestCode == LOGIN_REQUEST_CODE) {
             val response = IdpResponse.fromResultIntent(data)
-            if(resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 user = FirebaseAuth.getInstance().currentUser!!
                 Log.d("FirebaseAuth-User", "user: $user")
-                Log.d("FirebaseAuth-User", "uid: " + user!!.uid)
+                Log.d("FirebaseAuth-User", "uid: " + user.uid)
                 Log.d("FirebaseAuth-User", "response: $response")
                 Log.d("FirebaseAuth-User", "responseStr: " + response.toString().get(1))
                 Log.d("FirebaseAuth-User", "responseStr: ")
@@ -238,7 +251,7 @@ class SplashScreenActivity : AppCompatActivity() {
 //                    model.rating = 0.0
 //                    FirebaseDatabase.getInstance().getReference("DriverInfo").child(user.uid).setValue(model)
 //                }
-            }else{
+            } else {
                 Toast.makeText(this, "" + response!!.error!!.message, Toast.LENGTH_SHORT).show()
                 Log.d("SignIn Error", "" + response.error!!.message)
             }
